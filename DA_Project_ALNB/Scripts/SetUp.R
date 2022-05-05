5e915f16d211abedf1be9b70a34aeea5c8d81781
nbidet <32996297+nbidet@users.noreply.github.com>
2022-05-05 08:29:20 +0200

    Add files via upload

---
+++ DA_Project_ALNB/Scripts/SetUp.R
@@ -0,0 +1,16 @@
+#############################################
+## The following loads the needed packages ##
+#############################################
+
+# load the required packages
+packages <- c(
+  "tidyverse", "readxl", #for wrangling
+  "dplyr",  "lubridate","DataExplorer","summarytools","Hmisc",
+  "ggplot2", "naniar", "patchwork", "FactoMineR",
+  "factoextra", "corrplot", "GGally", #for plotting
+  "nnet", "caret", "neuralnet", # for Neural Network 
+  "randomForest","naivebayes",
+  "DT","tidyquant","ggthemes","RColorBrewer","corrr","ggcorrplot",
+  "kableExtra","DataExplorer","inspectdf")
+
+purrr::walk(packages, library, character.only = TRUE)
