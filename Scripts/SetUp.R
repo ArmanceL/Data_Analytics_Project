7552adfedd0d172fd3f7e50498fc576b57930d2b
ArmanceL <93270569+ArmanceL@users.noreply.github.com>
2022-05-27 18:37:36 +0200

    Update SetUp.R

--- Scripts/SetUp.R
+++ Scripts/SetUp.R
@@ -11,6 +11,6 @@ packages <- c(
   "nnet", "caret", "neuralnet", # for Neural Network 
   "randomForest","naivebayes",
   "DT","tidyquant","ggthemes","RColorBrewer","corrr","ggcorrplot","cowplot","grid","gridGraphics",
-  "kableExtra","DataExplorer","inspectdf","explore","Boruta","targets","visdat","VIM")
+  "kableExtra","DataExplorer","inspectdf","explore","Boruta","targets","visdat","VIM", "rpart")
 
 purrr::walk(packages, library, character.only = TRUE)
